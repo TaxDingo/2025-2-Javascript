704417b873bc760a9947202927a32952e8d27159
J. L. Medina. <pepe.ags.code@gmail.com>
2025-05-14 21:52:01 -0600

    .

--- Semana2/ejercicio2.js
+++ Semana2/ejercicio2.js
@@ -1,22 +1,41 @@
-const {ask} = require ('../helpers/input');
+const { ask } = require('../helpers/input');
 
 async function main() {
-    let option =''
-    let frutas =[]
+  let opcion = '';
+  let frutas = [];
 
-    while (option !==3){
-        option = await ask ('¿Que deseas hacer? 1.- Agregar una Fruta 2.- Eliminar una fruta 3.- Salir')
-        if (option === "1" ){
-            
-        }else if (option === "2" ){
+  while (opcion !== '3') {
+    opcion = await ask('¿Qué deseas hacer? \n1. Agregar una fruta \n2. Eliminar una fruta \n3. Salir\n');
+    if (opcion === '1') {
+      //Agregar
+      let frutaNueva = await ask('¿Qué fruta deseas agregar?: ');
+      frutas.push(frutaNueva);
+      console.log(`Fruta agregada: ${frutaNueva}`);
+      console.log('Frutas disponibles: ', frutas);
+    } else if (opcion === '2') {
+      //Eliminar
+      if (frutas.length === 0) {
+        console.log('No hay frutas disponibles');
+        continue;
+      } else {
+        let frutaEliminar = await ask('¿Qué fruta deseas eliminar?: ');
+        let index = frutas.indexOf(frutaEliminar);
 
-        } else if (option === "3" ){
-
+        if (index !== -1) {
+          frutas.splice(index, 1);
+          console.log(`${frutaEliminar} ha sido eliminada `)
         } else {
-            console.log ("Opción no valida")
+          console.log('Fruta no encontrada ');
         }
+        console.log('Frutas disponibles: ', frutas);
+      }
+    } else if (opcion === '3') {
+      //Salir
+      console.log('Gracias por usar el programa');
+    } else {
+      console.log('Opción no válida');
     }
-
-
-    
+  }
 }
+
+main();
